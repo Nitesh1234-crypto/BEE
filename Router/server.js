f64540576cff5f15ea101c87be9001592555f53d
Nitesh <Nitesh.e21@nsut.ac.in>
2023-12-07 14:50:07 +0530

    Router in express

--- Router/server.js
+++ Router/server.js
@@ -9,51 +9,13 @@ app.get("/",(req,res)=>{
     res.send("Router")
 })
 
-app.get("/posts",async(req,res)=>{
-    try {
-        let postdata=await POST.getpost()
-        if(postdata.lenght==0){
-           return res.send("No post found");
-        }
-        res.json(postdata);
-    } catch (error) {
-        res.send(error);
-    }
-  
-})
-app.post("/posts",async(req,res)=>{
-    let {postName,postImage,postCaption}=req.body;
-    let newPost={id:uuidv4(),postName:postName,postImage:postImage,postCaption:postCaption}
-    await POST.addpost(newPost)
-    res.redirect("/posts");
-})
-app.get("/posts/:id",async(req,res)=>{
-    try {
-        let {id}=req.params;
-        let post=await POST.getOnePost(id);
-        console.log(post);
-        if(post.lenght==0){
-           return res.send("No post found");
-        }
-        res.json(post);
-    
-    } catch (error) {
-        res.send(error);
-    }
- 
+// app.get("/posts",)
+// app.post("/posts",)
+// app.get("/posts/:id",  )
+// app.delete("/posts/:id",)
+// app.put("/posts/:id",)
+app.use("/posts",require("./routes/post"))
 
-})
-app.delete("/posts/:id",async(req,res)=>{
-   let {id} =req.params;
-   await POST.deletepost(id);
-   res.redirect("/posts");
-})
-app.put("/posts/:id",async(req,res)=>{
-     let {id} =req.params;
-     let {postName,postImage,postCaption} =req.body;
-     await POST.editpostput(id,{id,postName,postImage,postCaption})
-     res.redirect("/posts");
-})
 
 
 
