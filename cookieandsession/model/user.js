ae08f7d1d08fd6f1ee1b9cc5e24818d624d4d30f
Nitesh <Nitesh.e21@nsut.ac.in>
2024-01-09 20:14:49 +0530

    cookie session and populate

--- cookieandsession/model/user.js
+++ cookieandsession/model/user.js
@@ -1,8 +1,15 @@
 const mongoose =require("mongoose");
+const {Schema}=mongoose;
 
 let userSchema= new mongoose.Schema({
     username:String,
-    password:String
+    password:String,
+    blog:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"blog"
+        }
+    ]
 })
 
 module.exports= mongoose.model("User",userSchema);
--- cookieandsession/script.js
+++ cookieandsession/script.js
@@ -2,10 +2,11 @@ const express= require("express")
 const app = express()
 const path= require("path")
 const mongoose =require("mongoose");
-const session = require('express-session')
 const User =require("./model/user");
+const blog= require("./model/blog");
+const session = require('express-session')
 app.use(session({
-    secret: 'secret',
+    secret: 'keyboard cat',
     // resave: false,
     // saveUninitialized: true,
     // cookie: { secure: true }
@@ -13,17 +14,16 @@ app.use(session({
 app.use(express.static(path.join(__dirname,"static")))
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.set("view engine","hbs");
+app.set('view engine', 'hbs');
 function checkIsLoggedIn(req,res,next){
     if(req.session.isLoggedIn){
         next()
     }else{
         res.redirect("/login");
     }
-
 }
 app.get("/",checkIsLoggedIn,(req,res)=>{
-    res.render("home");
+    res.render("home",{user:req.session.user});
 })
 app.get("/login",(req,res)=>{
     res.render("login");
@@ -31,31 +31,57 @@ app.get("/login",(req,res)=>{
 app.get("/register",(req,res)=>{
     res.render("register");
 })
+
 app.post("/register",async(req,res)=>{
     const {username,password}=req.body;
-    let newUser =new User({username,password});
-    await newUser.save();
-    res.send("user registered successfully!!");
+    const newUser= new User({username,password});
+   await  newUser.save();
+   res.send("User registered successfully!!!")
+
 })
 app.post("/login",async(req,res)=>{
-    const {username,password}=req.body;
-    let user=await User.findOne({username:username});
-    if(user){
-        if(user.password!=password){
-            res.send("Invalid password!!")
-        }else{
-            req.session.isLoggedIn=true;
-            res.redirect("/");
-        }
-
+    const {username,password} =req.body;
+   let user=await User.findOne({username:username});
+   if(user){
+    if(user.password!=password){
+        res.send("Invalid password");
     }else{
-        res.send("user not found!!");
+        req.session.isLoggedIn=true;
+        req.session.user=user
+        res.redirect("/");
     }
+
+   }else{
+          res.send("user not found!!!");
+   }
+})
+app.post("/addblog",async(req,res)=>{
+    const {title,content} =req.body;
+    let newBlog= new blog({title,content,user:req.session.user._id});
+    await newBlog.save();
+    let user = await User.findOne({_id:req.session.user._id})
+    user.blog.push(newBlog._id);
+    await user.save();
+    res.send("done");
+    
+
+})
+app.get("/myblog",async(req,res)=>{
+    let user=await User.findOne({_id:req.session.user._id}).populate("blog");
+    console.log(user);
+    res.render("myblog",{blogs:user.blog,user:user});
+})
+app.get("/blogs",async(req,res)=>{
+    let blogs=await blog.find().populate("user");
+    console.log(blogs)
+    res.render("allblog",{blogs})
 })
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/g17session").then(()=>{
-    app.listen(3234,()=>{
-        console.log("server started at port 3334");
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/g26session").then(()=>{
+    app.listen(2345,()=>{
+        console.log("server started at port 2345");
       })
 })
